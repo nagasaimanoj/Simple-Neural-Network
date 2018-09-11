cd2e037c4b9de1877a3bf30998d2df8e30a7193e
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-09-11 22:05:31 +0530

    re-arranged

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-from numpy import exp, array, random, dot
+from numpy import array, dot, exp, random
 
 
 class NeuralNetwork():
     def __init__(self):
-        # Seed the random number generator, so it generates the same numbers
-        # every time the program runs.
+        # Seed the random number generator, so it generates the same numbers every time the program runs.
         random.seed(1)
 
         # We model a single neuron, with 3 input connections and 1 output connection.
-        # We assign random weights to a 3 x 1 matrix, with values in the range -1 to 1
-        # and mean 0.
+        # We assign random weights to a 3 x 1 matrix, with values in the range -1 to 1 and mean 0.
         self.synaptic_weights = 2 * random.random((3, 1)) - 1
 
     # The Sigmoid function, which describes an S shaped curve.
-    # We pass the weighted sum of the inputs through this function to
-    # normalise them between 0 and 1.
+    # We pass the weighted sum of the inputs through this function to normalise them between 0 and 1.
     def __sigmoid(self, x):
         return 1 / (1 + exp(-x))
 
@@ -27,18 +24,18 @@ class NeuralNetwork():
     # We train the neural network through a process of trial and error.
     # Adjusting the synaptic weights each time.
     def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
-        for iteration in xrange(number_of_training_iterations):
+        for iteration in range(number_of_training_iterations):
             # Pass the training set through our neural network (a single neuron).
             output = self.think(training_set_inputs)
 
-            # Calculate the error (The difference between the desired output
-            # and the predicted output).
+            # Calculate the error (The difference between the desired output and the predicted output).
             error = training_set_outputs - output
 
             # Multiply the error by the input and again by the gradient of the Sigmoid curve.
             # This means less confident weights are adjusted more.
             # This means inputs, which are zero, do not cause changes to the weights.
-            adjustment = dot(training_set_inputs.T, error * self.__sigmoid_derivative(output))
+            adjustment = dot(training_set_inputs.T, error *
+                             self.__sigmoid_derivative(output))
 
             # Adjust the weights.
             self.synaptic_weights += adjustment
@@ -51,14 +48,13 @@ class NeuralNetwork():
 
 if __name__ == "__main__":
 
-    #Intialise a single neuron neural network.
+    # Intialise a single neuron neural network.
     neural_network = NeuralNetwork()
 
-    print "Random starting synaptic weights: "
-    print neural_network.synaptic_weights
+    print("Random starting synaptic weights: ")
+    print(neural_network.synaptic_weights)
 
-    # The training set. We have 4 examples, each consisting of 3 input values
-    # and 1 output value.
+    # The training set. We have 4 examples, each consisting of 3 input values and 1 output value.
     training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
     training_set_outputs = array([[0, 1, 1, 0]]).T
 
@@ -66,9 +62,9 @@ if __name__ == "__main__":
     # Do it 10,000 times and make small adjustments each time.
     neural_network.train(training_set_inputs, training_set_outputs, 10000)
 
-    print "New synaptic weights after training: "
-    print neural_network.synaptic_weights
+    print("New synaptic weights after training: ")
+    print(neural_network.synaptic_weights)
 
     # Test the neural network with a new situation.
-    print "Considering new situation [1, 0, 0] -> ?: "
-    print neural_network.think(array([1, 0, 0]))
+    print("Considering new situation [1, 0, 0] -> ?: ")
+    print(neural_network.think(array([1, 0, 0])))
