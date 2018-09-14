4b332db5f6ab77362dbbd5f4856119ab3978d2ff
InnovationLab NewMacMini <innovationlab@InnovationLabNewMacMini.local>
2018-09-14 17:44:19 +0530

    variable names changed

--- short_version.py
+++ short_version.py
@@ -1,29 +1,29 @@
 from numpy import array, dot, exp
 
-training_set_inputs = array([
+input_set = array([
     [0, 0, 1],
     [1, 1, 1],
     [1, 0, 1],
     [0, 1, 1]
 ])
 
-training_set_outputs = array([
+output_set = array([
     [0],
     [1],
     [1],
     [0]
 ])
 
-synaptic_weights = array([
+weights = array([
     [0.],
     [0.],
     [0.]
 ])
 
 for iteration in range(10000):
-    output = 1 / (1 + exp(-(dot(training_set_inputs, synaptic_weights))))
+    output = 1 / (1 + exp(-(dot(input_set, weights))))
 
-    synaptic_weights += dot(training_set_inputs.T,
-                            (training_set_outputs - output) * output * (1 - output))
+    weights += dot(input_set.T,
+                            (output_set - output) * output * (1 - output))
 
-print(1 / (1 + exp(-(dot(array([1, 0, 0]), synaptic_weights)))))
+print(1 / (1 + exp(-(dot(array([1, 0, 0]), weights)))))
